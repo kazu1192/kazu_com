0cb3994c4e4b3014a23bf05f2842c8f1a64355dc
kazu1192 <dye.deep.red@gmail.com>
2019-06-20 22:52:43 +0900

    feature: articles_controller へ index を追加

--- backend/app/controllers/api/v1/articles_controller.rb
+++ backend/app/controllers/api/v1/articles_controller.rb
@@ -1,2 +1,6 @@
 class Api::V1::ArticlesController < ApplicationController
+  def index
+    articles = Article.order(created_at: :desc)
+    render json: { status: 'SUCCESS', message: 'loaded posts', data: posts }
+  end
 end
